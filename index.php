b4dd9389a3767da39559f74a75b1ee0a61c19904
Celine Leano <cemleano@gmail.com>
2019-02-10 14:36:04 -0500

    Created route and view for homepage

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Celine Leano and Adolfo Gonzalez
+ * 2/10/2019
+ * 328/final-project/index.php
+ * Fat-Free Routing
+ */
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require autoload
+require_once('vendor/autoload.php');
+
+// create an instance of the Base class
+$f3 = Base::instance();
+
+// turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+// define a default route
+$f3->route('GET|POST /', function($f3) {
+    $f3->set("title", "Key Management");
+
+    $template = new Template();
+    echo $template->render("views/home.html");
+});
+
+// define login route
+$f3->route('GET|POST /login', function($f3) {
+   $f3->set("title", "Employee Login");
+
+   $template = new Template();
+   echo $template->render("views/login.html");
+});
+
+// run fat free
+$f3->run();
