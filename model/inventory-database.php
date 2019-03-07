3e7c4df052312b8d6acab2609d329e83266cb086
adolfo <agonzalez15@mail.greenriver.edu>
2019-03-07 12:36:42 -0500

    updated name

--- model/inventory-database.php
+++ model/inventory-database.php
@@ -27,139 +27,181 @@
 	//Connect to Inventory Database
 	require_once('/home/agonzale/config.php');
 	
-		function connect()
-		{
-			try{
-				//Instantiate a db object
-				$dbh=new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
-				return $dbh;
-			}
-			catch(PDOException $ex){
-				echo $ex->getMessage();
-				return FALSE;
-			}
+	/**
+	 * This method connect to the inventory database.
+	 * @return bool|\PDO
+	 */
+	function connect()
+	{
+		try{
+			//Instantiate a db object
+			$dbh=new PDO(DB_DSN,DB_USERNAME,DB_PASSWORD);
+			return $dbh;
 		}
-		
-		//Add  New Car
-		function addCar($stock,$make,$model,$year,$updatedBy,$timeStamp,$notes)
-		{
-			global $dbh;
-			//connect to database
-			$dbh->connect();
-			//1. define query
-			$sql="INSERT INTO inventory(stock, make, model, year, updatedBy, timestamp, notes) VALUES(:stock, :make, :model,
+		catch(PDOException $ex){
+			echo $ex->getMessage();
+			return FALSE;
+		}
+	}
+	
+	//Add  New Car
+	/**
+	 * This method creates a new car object with the given parametes
+	 * @param $stock
+	 * @param $make
+	 * @param $model
+	 * @param $year
+	 * @param $updatedBy
+	 * @param $timeStamp
+	 * @param $notes
+	 *
+	 * @return bool
+	 */
+	function addCar($stock,$make,$model,$year,$updatedBy,$timeStamp,$notes)
+	{
+		global $dbh;
+		//connect to database
+		$dbh->connect();
+		//1. define query
+		$sql="INSERT INTO inventory(stock, make, model, year, updatedBy, timestamp, notes) VALUES(:stock, :make, :model,
 										:year, :updatedBy, :timestamp, :notes)";
-			
-			//2. prepare the statement
-			$statement=$dbh->prepare($sql);
-			
-			//3. bind parameters
-			$statement->bindParam(':stock',$stock,PDO::PARAM_STR);
-			$statement->bindParam(':make',$make,PDO::PARAM_STR);
-			$statement->bindParam(':model',$model,PDO::PARAM_STR);
-			$statement->bindParam(':year',$year,PDO::PARAM_STR);
-			$statement->bindParam(':updatedBy',$updatedBy,PDO::PARAM_STR);
-			$statement->bindParam(':timestamp',$timestamp,PDO::PARAM_STR);
-			$statement->bindParam(':notes',$notes,PDO::PARAM_STR);
-			
-			//4. execute
-			return $statement->execute();
-		}
 		
-		//get all cars
-		function getCars()
-		{
-			//1. connect to database
-			global $dbh;
-			$dbh->connect();
-			
-			//2. define query
-			$sql="SELECT * FROM inventory";//maybe group by status or date
-			
-			//3. prepare statement
-			$statement=$dbh->prepare($sql);
-			
-			//4. execute statement
-			$statement->execute();
-			
-			//get results
-			$results=$statement->fetch(PDO::FETCH_ASSOC);
-			
-			//return results
-			return $results;
-		}
+		//2. prepare the statement
+		$statement=$dbh->prepare($sql);
 		
-		//Get Car Info/Status
-		function getByStock($stock)
-		{
-			//1. connect to database
-			global $dbh;
-			$dbh->connect();
-			
-			//2. define query
-			$sql="SELECT * FROM inventory  WHERE stock = '$stock'";
-			
-			//3. prepare statement
-			$statement=$dbh->prepare($sql);
-			
-			//4. statement execute
-			$statement->execute();
-			
-			//get results
-			$results=$statement->fetch(PDO::FETCH_ASSOC);
-			
-			//return results
-			return $results;
-		}
+		//3. bind parameters
+		$statement->bindParam(':stock',$stock,PDO::PARAM_STR);
+		$statement->bindParam(':make',$make,PDO::PARAM_STR);
+		$statement->bindParam(':model',$model,PDO::PARAM_STR);
+		$statement->bindParam(':year',$year,PDO::PARAM_STR);
+		$statement->bindParam(':updatedBy',$updatedBy,PDO::PARAM_STR);
+		$statement->bindParam(':timestamp',$timestamp,PDO::PARAM_STR);
+		$statement->bindParam(':notes',$notes,PDO::PARAM_STR);
 		
-		//Update Car Status
-		function updateStatus($stock,$status)
-		{
-			//1. connect to database
-			global $dbh;
-			$dbh->connect();
-			
-			//2. define query
-			$sql="UPDATE  inventory SET status = '$status' WHERE stock = '$stock'";
-			
-			//3. prepare statement
-			$statement=$dbh->prepare($sql);
-			
-			//4. statement execute
-			return $statement->execute();
-			
-		}
+		//4. execute
+		return $statement->execute();
+	}
+	
+	//get all cars
+	/**
+	 * @return mixed
+	 */
+	function getCars()
+	{
+		//1. connect to database
+		global $dbh;
+		$dbh->connect();
 		
-		function updateNotes($stock,$notes)
-		{
-			//1. connect to database
-			global $dbh;
-			$dbh->connect();
-			
-			//2. define query
-			$sql="UPDATE  inventory SET notes = '$notes' WHERE stock = '$stock'";
-			
-			//3. prepare statement
-			$statement=$dbh->prepare($sql);
-			
-			//4. statement execute
-			return $statement->execute();
-		}
+		//2. define query
+		$sql="SELECT * FROM inventory";//maybe group by status or date
 		
-		//Remove Car
-		function removeCar($stock)
-		{
-			//1. connect to database
-			global $dbh;
-			$dbh->connect();
-			
-			//2. define query
-			$sql="DELETE FROM  inventory  WHERE stock = '$stock'";
-			
-			//3. prepare statement
-			$statement=$dbh->prepare($sql);
-			
-			//4. statement execute
-			return $statement->execute();
-			
-		}
+		//3. prepare statement
+		$statement=$dbh->prepare($sql);
+		
+		//4. execute statement
+		$statement->execute();
+		
+		//get results
+		$results=$statement->fetch(PDO::FETCH_ASSOC);
+		
+		//return results
+		return $results;
+	}
+	
+	//Get Car Info/Status
+	/**
+	 * This method retrives a car with a given stock number.
+	 * @param $stock
+	 *
+	 * @return mixed
+	 */
+	function getByStock($stock)
+	{
+		//1. connect to database
+		global $dbh;
+		$dbh->connect();
+		
+		//2. define query
+		$sql="SELECT * FROM inventory  WHERE stock = '$stock'";
+		
+		//3. prepare statement
+		$statement=$dbh->prepare($sql);
+		
+		//4. statement execute
+		$statement->execute();
+		
+		//get results
+		$results=$statement->fetch(PDO::FETCH_ASSOC);
+		
+		//return results
+		return $results;
+	}
+	
+	//Update Car Status
+	/**
+	 * @param $stock
+	 * @param $status
+	 *
+	 * @return bool
+	 */
+	function updateStatus($stock,$status)
+	{
+		//1. connect to database
+		global $dbh;
+		$dbh->connect();
+		
+		//2. define query
+		$sql="UPDATE  inventory SET status = '$status' WHERE stock = '$stock'";
+		
+		//3. prepare statement
+		$statement=$dbh->prepare($sql);
+		
+		//4. statement execute
+		return $statement->execute();
+		
+	}
+	
+	/**
+	 * @param $stock
+	 * @param $notes
+	 *
+	 * @return bool
+	 */
+	function updateNotes($stock,$notes)
+	{
+		//1. connect to database
+		global $dbh;
+		$dbh->connect();
+		
+		//2. define query
+		$sql="UPDATE  inventory SET notes = '$notes' WHERE stock = '$stock'";
+		
+		//3. prepare statement
+		$statement=$dbh->prepare($sql);
+		
+		//4. statement execute
+		return $statement->execute();
+	}
+	
+	//Remove Car
+	/**
+	 * @param $stock
+	 *
+	 * @return bool
+	 */
+	function removeCar($stock)
+	{
+		//1. connect to database
+		global $dbh;
+		$dbh->connect();
+		
+		//2. define query
+		$sql="DELETE FROM  inventory  WHERE stock = '$stock'";
+		
+		//3. prepare statement
+		$statement=$dbh->prepare($sql);
+		
+		//4. statement execute
+		return $statement->execute();
+		
+	}
