4a2c49a23c3dae638e09ee1f6760f122d3ddb751
adolfo <agonzalez15@mail.greenriver.edu>
2019-03-10 20:35:05 -0400

    added valid data function

--- model/validation.php
+++ model/validation.php
@@ -1,9 +1,9 @@
 <?php
-/**
- * Celine Leano and Adolfo Gonzalez
- * 2/10/2019
- * 328/final-project/validation.php
- * Fat-Free Routing
- */
-
-//validation page
+	/**
+	 * Celine Leano and Adolfo Gonzalez
+	 * 2/10/2019
+	 * 328/final-project/validation.php
+	 * Fat-Free Routing
+	 */
+	
+	//validation page
