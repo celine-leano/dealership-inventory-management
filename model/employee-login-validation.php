1fb80549425626f2243d64bcfd13030b08140fef
Celine Leano <cemleano@gmail.com>
2019-03-12 16:48:48 -0400

    Changed the way error messages are displayed

--- model/employee-login-validation.php
+++ model/employee-login-validation.php
@@ -10,28 +10,29 @@ global $f3;
 
 // login validation
 $isValid = true;
+$f3->set("errorEmployeeLogin", null);
 
 if (!empty($_POST)) {
-    // validate username field
+    // validate username and password field
     $username = $_POST['username'];
+    $password = $_POST['password'];
     $f3->set("username", $username);
+    $f3->set("password", $password);
+
     if (empty($_POST['username'])) {
-        $f3->set("errorUsername", "Please enter a username");
+        $f3->set("errorEmployeeLogin", "Please enter a username");
         $isValid = false;
     } else if ($username == "admin") {
-        $f3->set("errorUsername", "Please use the admin login");
+        $f3->set("errorEmployeeLogin", "Please use the admin login");
         $isValid = false;
     } else if ($username != "lmemployee") {
-        $f3->set("errorUsername", "Invalid username");
+        $f3->set("errorEmployeeLogin", "Invalid username");
+        $isValid = false;
+    } else if (empty($_POST['password'])) {
+        $f3->set("errorEmployeeLogin", "Please enter a password");
         $isValid = false;
     }
 
-// validate password field
-    $password = $_POST['password'];
-    if (empty($_POST['password'])) {
-        $f3->set("errorPassword", "Please enter a password");
-        $isValid = false;
-    }
 
     if ($isValid) {
         // check that username and password are valid based on the info
@@ -41,7 +42,7 @@ if (!empty($_POST)) {
         if ($success) {
             $f3->reroute("stock");
         } else {
-            $f3->set("errorLogin", "Username or password is incorrect");
+            $f3->set("errorEmployeeLogin", "Password is incorrect");
         }
     }
 }
