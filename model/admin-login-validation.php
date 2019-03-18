a3e7212e70cb094a9014bd6350fd213cc3c3cfd7
Celine Leano <cemleano@gmail.com>
2019-03-17 22:21:02 -0400

    Changed styling for admin login errors messages

--- model/admin-login-validation.php
+++ model/admin-login-validation.php
@@ -9,39 +9,32 @@
 global $f3;
 // login flag for validation
 $isValid = TRUE;
-$f3 -> set("errorPassword", NULL);
-if(!empty($_POST))
-{
+$f3->set("errorAdminLogin", NULL);
+if (!empty($_POST)) {
     //validate username and password field
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $f3 -> set("username", $username);
-    $f3 -> set("password", $password);
-    if(empty($_POST['username']))
-    {
-        $f3 -> set("errorUsername", "Please enter a username");
+    $f3->set("username", $username);
+    $f3->set("password", $password);
+    if (empty($_POST['username'])) {
+        $f3->set("errorAdminLogin", "Please enter a username");
         $isValid = FALSE;
     }
-    if(empty($_POST['password']))
-    {
-        $f3 -> set("errorPassword", "Please enter a password");
+    if (empty($_POST['password'])) {
+        $f3->set("errorAdminLogin", "Please enter a password");
         $isValid = FALSE;
     }
-    if($isValid)
-    {
+    if ($isValid) {
         // check that username and password are valid based on the info
         // in the database
         $password = strtolower($password);
         $password = SHA1($password);
         $success = adminLogin($username, $password);
-        if($success)
-        {
+        if ($success) {
             $_SESSION['username'] = $username;
-            $f3 -> reroute("admin/tools");
-        }
-        else
-        {
-            $f3 -> set("errorLogin", "Password is incorrect");
+            $f3->reroute("admin/tools");
+        } else {
+            $f3->set("errorAdminLogin", "Username and/or password is incorrect");
         }
     }
 }
