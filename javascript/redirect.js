5dc8a0e550ccafe17ef4c973ad5a36b3b6881d5e
Celine Leano <cemleano@gmail.com>
2019-03-18 16:58:56 -0400

    Added comments and headers

--- javascript/redirect.js
+++ javascript/redirect.js
@@ -1,3 +1,10 @@
+/*
+Celine Leano and Adolfo Gonzalez
+3/17/2019
+328/final-project/javascript/redirect.js
+Redirects user after logging out
+ */
+
 var count = 1;
 var redirect = "http://cleano.greenriverdev.com/328/final-project";
 
